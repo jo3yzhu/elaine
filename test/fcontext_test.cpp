f4277e05790f8a4b9e0013f487d557e9265241d6
jo3zhu <jo3yzhu@gmail.com>
2021-03-01 11:00:13 +0800

    add fcontext comparision with ucontext and libcontext

---
+++ test/fcontext_test.cpp
@@ -0,0 +1,66 @@
+#include "libcontext.h"
+#include <ucontext.h>
+
+#include <stdlib.h>
+#include <chrono>
+#include <iostream>
+
+fcontext_t main_fctx, co_fctx;
+ucontext_t main_uctx, co_uctx;
+
+void* spf, * spu;
+size_t sp_size = 8192;
+const int switch_times = 1000000;
+
+
+void f_co(intptr_t) {
+    for (int i = 0; i < switch_times; ++i) {
+        jump_fcontext(&co_fctx, main_fctx, 0, false);
+    }
+}
+
+void ftest() {
+    spf = malloc(sp_size);
+    co_fctx = make_fcontext(spf, sp_size, f_co);
+
+    for (int i = 0; i < switch_times; ++i) {
+        jump_fcontext(&main_fctx, co_fctx, 0, false);
+    }
+}
+
+void u_co() {
+    for (int i = 0; i < switch_times; ++i) {
+        swapcontext(&co_uctx, &main_uctx);
+    }
+}
+
+void utest() {
+    spu = malloc(sp_size);
+    getcontext(&co_uctx);
+    co_uctx.uc_stack.ss_sp = spu;
+    co_uctx.uc_stack.ss_size = sp_size;
+    co_uctx.uc_link = NULL;
+
+    makecontext(&co_uctx, u_co, 0);
+    for (int i = 0; i < switch_times; ++i) {
+        swapcontext(&main_uctx, &co_uctx);
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    auto t1 = std::chrono::system_clock::now();
+    ftest();
+    auto t2 = std::chrono::system_clock::now();
+    auto d1 = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+    std::cout << "fcontext: " << double(d1.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << " second" << std::endl;
+
+    utest();
+    auto t3 = std::chrono::system_clock::now();
+    auto d2 = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2);
+    std::cout << "ucontext: " << double(d2.count()) * std::chrono::microseconds::period::num / std::chrono::microseconds::period::den << " second" << std::endl;
+
+    // ucontext is 10X slower compared with fcontext
+
+    return 0;
+}
---
+++ test/liburing_test.cpp
@@ -0,0 +1,123 @@
+#include "liburing_test.h"
+
+static const char* op_strs[] = {
+    "IORING_OP_NOP",
+    "IORING_OP_READV",
+    "IORING_OP_WRITEV",
+    "IORING_OP_FSYNC",
+    "IORING_OP_READ_FIXED",
+    "IORING_OP_WRITE_FIXED",
+    "IORING_OP_POLL_ADD",
+    "IORING_OP_POLL_REMOVE",
+    "IORING_OP_SYNC_FILE_RANGE",
+    "IORING_OP_SENDMSG",
+    "IORING_OP_RECVMSG",
+    "IORING_OP_TIMEOUT",
+    "IORING_OP_TIMEOUT_REMOVE",
+    "IORING_OP_ACCEPT",
+    "IORING_OP_ASYNC_CANCEL",
+    "IORING_OP_LINK_TIMEOUT",
+    "IORING_OP_CONNECT",
+    "IORING_OP_FALLOCATE",
+    "IORING_OP_OPENAT",
+    "IORING_OP_CLOSE",
+    "IORING_OP_FILES_UPDATE",
+    "IORING_OP_STATX",
+    "IORING_OP_READ",
+    "IORING_OP_WRITE",
+    "IORING_OP_FADVISE",
+    "IORING_OP_MADVISE",
+    "IORING_OP_SEND",
+    "IORING_OP_RECV",
+    "IORING_OP_OPENAT2",
+    "IORING_OP_EPOLL_CTL",
+    "IORING_OP_SPLICE",
+    "IORING_OP_PROVIDE_BUFFERS",
+    "IORING_OP_REMOVE_BUFFERS",
+    "IORING_OP_TEE",
+    "IORING_OP_SHUTDOWN",
+    "IORING_OP_RENAMEAT",
+    "IORING_OP_UNLINKAT",
+    "IORING_OP_MKDIRAT",
+};
+
+void env_support_check() {
+    struct utsname u;
+    uname(&u);
+    printf("You are running kernel version: %s\n", u.release);
+    struct io_uring_probe* probe = io_uring_get_probe();
+    printf("Report of your kernel's list of supported io_uring operations:\n");
+    for (char i = 0; i < IORING_OP_LAST; i++) {
+        printf("%s: ", op_strs[i]);
+        if (io_uring_opcode_supported(probe, i))
+            printf("yes.\n");
+        else
+            printf("no.\n");
+    }
+    free(probe);
+}
+
+void timerfd_support_check() {
+    int timer_internal_second = 1;
+    int timer_expired_times = 10;
+    int timer_expired_seq = 0;
+    int timer_unique_id[] = { 0,1,2,3,4,5,6,7,8,9 };
+
+    // init timerfd, timerfd must be in block mode when work with io_uring
+    int tfd = timerfd_create(CLOCK_REALTIME, 0);
+
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+
+    // set timer internal and firstly expired time
+    struct itimerspec new_value;
+    new_value.it_value.tv_sec = now.tv_sec + timer_internal_second;
+    new_value.it_value.tv_nsec = now.tv_nsec;
+    new_value.it_interval.tv_sec = timer_internal_second;
+    new_value.it_interval.tv_nsec = 0;
+
+    // init io_uring
+    struct io_uring ring;
+    io_uring_queue_init(1, &ring, 0);
+    struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
+
+    // submit read request to io_uring
+    uint64_t expired_times = 0;
+    io_uring_prep_read(sqe, tfd, &expired_times, sizeof(uint64_t), 0);
+    io_uring_sqe_set_data(sqe, &timer_unique_id[timer_expired_seq++]);
+    io_uring_submit(&ring);
+
+    // before timer start
+    auto before = std::chrono::system_clock::now().time_since_epoch();
+    auto before_ms = std::chrono::duration_cast<std::chrono::microseconds>(before);
+    printf("timer firstly started at: %ld\n", before_ms.count());
+
+    // start timer
+    timerfd_settime(tfd, TFD_TIMER_ABSTIME, &new_value, NULL);
+
+    // wait for expired timer
+    for (int count = 0; count < timer_expired_times; ++count) {
+        struct io_uring_cqe* cqe;
+        io_uring_wait_cqe(&ring, &cqe);
+        int seq = *(int*)io_uring_cqe_get_data(cqe);
+        
+        // cqe must be marked as seen if application has processed it
+        // if not, the result of io_uring_wait_cqe would always be the head node of CQ and seems like never removed
+        io_uring_cqe_seen(&ring, cqe); 
+
+        auto after = std::chrono::system_clock::now().time_since_epoch();
+        auto after_ms = std::chrono::duration_cast<std::chrono::microseconds>(after);
+        printf("timer exipred at: %ld and seq is: %d\n", after.count(), seq);
+
+        struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
+        io_uring_prep_read(sqe, tfd, &expired_times, sizeof(uint64_t), 0);
+        io_uring_sqe_set_data(sqe, &timer_unique_id[timer_expired_seq++]);
+        io_uring_submit(&ring);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    env_support_check();
+    timerfd_support_check();
+    return 0;
+}
